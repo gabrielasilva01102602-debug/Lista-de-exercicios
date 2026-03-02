ae5a3e6e15267bad4c20b601f1c06ce5ed3bb5ba
gabrielasilva01102602-debug <gabriela.silva01102602@sesisenaipr.org.br>
2026-03-02 12:39:17 +0000

    lista de exercicios

---
+++ ex14.js
@@ -0,0 +1,4 @@
+//14 -Criar um programa que mostre um desconto de 5% paracompras maiores que R$100
+//15 -Criar um prograa que classifique uma pessoa como Criança, Adolecente ou adulto de acordo com a idade
+//16 -Criar um programa que mostre qual tipo de triângulo de acordo com seus lados (Equilatero,Isóceles e Escaleno)
+//17 -Criar um programa que ostre
